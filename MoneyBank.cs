70f1802e0ef9a3e8a5b126ec746844b3d8421208
mikeErmantrautKuznetsov <128633238+mikeErmantrautKuznetsov@users.noreply.github.com>
2024-12-10 01:52:25 +0300

    Second

---
+++ MoneyBank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceOOP
+{
+    public class MoneyService
+    {
+        private int _balanceService;
+        private int _fineService;
+        private int _serviceRepair;
+
+        public int BalanceService { get; private set; } = 100000;
+        public int FineService { get; private set; } = 1000;
+        public int ServiceRepair { get; private set; } = 2000;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,75 +8,6 @@
     //За каждую удачную починку вы получаете выплату за ремонт, которая указана в чек-листе починки.
     //Класс Деталь не может содержать значение “количество”. Деталь всего одна, за количество отвечает тот, кто хранит детали.При необходимости можно создать дополнительный класс для конкретной детали и работе с количеством.
 
-    public class MoneyService
-    {
-        private int _balanceService = 100000;
-        private int _sactionService = 1000;
-        private int _repeatService = 2000;
-
-        public int BalanceService { get { return _balanceService; } set { _balanceService = value; } }
-        public int SactionService { get { return _sactionService; } set { _sactionService = value; } }
-        public int RepeatService { get { return _repeatService; } set { _repeatService = value; } }
-
-    }
-
-    public class Warehouse
-    {
-        MoneyService moneyService = new MoneyService();
-        ServiceAuto serviceAuto1 = new ServiceAuto();
-
-        Dictionary<string, int> details = new Dictionary<string, int>()
-        {
-            {"двигатель", 5000},
-            {"тормаза", 1000},
-            {"генератор", 2000},
-            {"аккамулятор", 500},
-            {"бензобак", 1500}
-        };
-
-        public void Cheack()
-        {
-            foreach (KeyValuePair<string, int> detailsValues in details)
-            {
-                Console.WriteLine(detailsValues);
-            }
-        }
-
-        public void RenovatCar()
-        {
-            string needDetails = Console.ReadLine();
-            Console.WriteLine();
-            foreach (KeyValuePair<string, int> detail in details)
-                if (needDetails == detail.Key)
-                {
-                    Console.WriteLine($"Цена за ремонт: {moneyService.RepeatService}руб.\n" +
-                        $"Цена детали: {detail.Value}руб. Итог {moneyService.RepeatService + detail.Value + moneyService.BalanceService}руб.");
-                    details.Remove(detail.Key);
-                    break;
-                }
-                else if (needDetails != detail.Key)
-                {
-                    serviceAuto1.SactionClient();
-                    Console.WriteLine($"Баланс: {moneyService.BalanceService - moneyService.SactionService}");
-                    break;
-                }
-        }
-    }
-
-
-    public class ServiceAuto
-    {
-        public void RepairCar()
-        {
-            Console.WriteLine("Ремонт выполнен успешно.");
-        }
-
-        public void SactionClient()
-        {
-            Console.WriteLine("В ремонте отказано.");
-        }
-    }
-
     public class Program
     {
         static void Main(string[] args)
@@ -87,7 +18,7 @@
 
             Console.WriteLine("Склад.");
             Console.WriteLine();
-            warehouse.Cheack();
+            warehouse.Check();
             Console.WriteLine();
 
             Console.WriteLine("Выберите команду: 1 - Ремонт машины. 2 отказ в ремонте.");
@@ -99,12 +30,12 @@
                     case 1:
                         Console.WriteLine();
                         Console.WriteLine("Напишите сломанную деталь:");
-                        warehouse.RenovatCar();
+                        warehouse.RenovateCar();
                         serviceAuto.RepairCar();
                         break;
                     case 2:
-                        serviceAuto.SactionClient();
-                        Console.WriteLine($"Баланс: {moneyService.BalanceService - moneyService.SactionService}");
+                        serviceAuto.ClientFine();
+                        Console.WriteLine($"Баланс: {moneyService.BalanceService - moneyService.FineService}");
                         break;
                     default:
                         Console.WriteLine("К сожалению у нас нет деталей для ремонта. Бей отказ из-за нарушений правил эксплуатациию.");
