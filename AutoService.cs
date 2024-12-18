144af987b24476601d545b10db7728df5eb5c4a4
mikeErmantrautKuznetsov <128633238+mikeErmantrautKuznetsov@users.noreply.github.com>
2024-12-19 00:51:02 +0300

    next

--- AutoService.cs
+++ AutoService.cs
@@ -7,27 +7,17 @@
 
         public void RepairCar(string needDetails)
         {
-            foreach (KeyValuePair<string, int> detail in warehouse._details)
-                if (needDetails == detail.Key)
-                {
-                    Console.WriteLine($"Цена за ремонт: {moneyService.ServiceRepair}руб.\n" +
-                        $"Цена детали: {detail.Value}руб. Итог {moneyService.BalanceService = moneyService.ServiceRepair + detail.Value + moneyService.BalanceService}руб.");
-                    warehouse._details.Remove(detail.Key);
-                    Console.WriteLine("Ремонт выполнен успешно.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Такой детали нет. В ремонте отказано.");
-                    Console.WriteLine($"Баланс: {moneyService.BalanceService = moneyService.BalanceService - moneyService.FineService}");
-                    ClientFine();
-                    break;
-                }
-
+            if (needDetails != null)
+            {
+                warehouse.RemoveDetail(needDetails);
+                Console.WriteLine("Ремонт выполнен успешно.");
+            }
         }
 
         public void ClientFine()
         {
+            Console.WriteLine("Такой детали нет. В ремонте отказано.");
+            Console.WriteLine($"Баланс: {moneyService.BalanceService = moneyService.BalanceService - moneyService.FineService}");
             Console.WriteLine("В ремонте отказано.");
         }
 
--- WareHouse.cs
+++ WareHouse.cs
@@ -1,9 +1,10 @@
-﻿
-namespace ServiceOOP
+﻿namespace ServiceOOP
 {
     public class Warehouse
     {
-        public Dictionary<string, int> _details = new Dictionary<string, int>()
+        MoneyService moneyService = new MoneyService();
+
+        Dictionary<string, int> _details = new Dictionary<string, int>()
         {
             {"двигатель", 5000},
             {"тормаз", 1000},
@@ -18,6 +19,18 @@ namespace ServiceOOP
             {
                 Console.WriteLine(detailsValues);
             }
+        }
+
+        public void RemoveDetail(string needDetails)
+        {
+            foreach (KeyValuePair<string, int> detail in _details)
+                if (needDetails == detail.Key)
+                {
+                    Console.WriteLine($"Цена за ремонт: {moneyService.ServiceRepair}руб.\n" +
+                        $"Цена детали: {detail.Value}руб. Итог {moneyService.BalanceService = moneyService.ServiceRepair + detail.Value + moneyService.BalanceService}руб.");
+                    _details.Remove(detail.Key);
+                    break;
+                }
         }
     }
 }
